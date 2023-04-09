f7fd9b66adc906ce4fda1e4be58a43687c8638df
Ceres00 <101172927+Ceres00@users.noreply.github.com>
2023-04-09 17:12:30 +0300

    First level continued, test map integrated.

--- Assets/Scripts/CameraTP.cs
+++ Assets/Scripts/CameraTP.cs
@@ -6,12 +6,13 @@ public class CameraTP : MonoBehaviour
 {
     public GameObject Camera;
     public GameObject Player;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
             Camera.transform.position = new Vector3(20f, 0f, -10f);
-            Player.transform.position = new Vector3(11.86f, -0.77f, 0f);
+            Player.transform.position = new Vector3(12f, 2f, 0f);
             
         }
     }
